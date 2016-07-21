6e7924b1923247e6ad14b4c76bb0c42dcd1d88b1
Benjamin Schnabel <Benjamin-777@gmx.de>
2016-07-21 12:41:39 +0200

    before moving directory

--- js/mainmenu.js
+++ js/mainmenu.js
@@ -1,67 +1,36 @@
-/**
- * 
- */
+		<script type="text/javascript">
 
 //scripts for the main menu
 
-	$(document).ready(function() {
 		//Initialisieren
 		//alle submenus loeschen -> macht das css
+//$(function() {
+//		$('.mainmenu ul li').hover(function () {
+//    	// hover in
+//    	console.log('Hover In');
+//		var a = $(this).parents();
+//		//anderes submenu anzeigen
+//    	console.log(a[1].nodeName);
 
-		$('.mainmenu ul li').hover(function () {
-		//hover fadeIn
-		//console.log("Hover In");
-		//aktives submenu loeschen
-		//$('.active li ul.submenu').css({
-
-		//anderes submenu anzeigen
-		//$(this).fadeOut('fast');
-		a = $(this).parents();
-		console.log(a[1].nodeName);
-		if($(this).hasClass('parent')) {
-		$('ul.submenu').css({
-		'display' : 'none',
-		'opacity' : '0.0'
-		});
-
-		$(this).children('ul.submenu').css({
-		'display': 'block'
-		}).fadeTo('', '1.0');
-		}//else{
-		//$(this).children('ul.submenu').css({
-		//'display' : 'none',
-		//'opacity' : '0.0'
-		//});
-		//}
-		}, function () {
-		//hover fadeOut
-		//	console.log("Hover Out");
-		//if(!$(this).hasClass('parent') && (!$(this).parents('.submenu'))) {
-		if('.mainmenu ul li :not(ul.submenu)'){
-		$('ul.submenu').css({
-		'display' : 'none',
-		'opacity' : '0.0'
-		});
-		}
-		//   $('ul.submenu').css({
-		//   'display': 'block'
-		//	}).fadeTo('', '0.0');
-
-		//aktives Submenu anzeigen
-		// $('.active li ul.submenu').css({
-		//     'display': 'block'
-		// }) .fadeTo('', '1.0');
-		});
-		});
-
-		//display 1st submenu
-		//$(document).ready(function() {
-		//	console.log("submenu");
-		//	$(".active li ul.submenu").css({
-		//    "display" : "block", "opacity" : "1.0"});
-		//});
+//    	if ($(this).hasClass('parent')) {
+//			$(this).children('ul.submenu').css({
+//			'display': 'block'
+//			}).fadeTo('', '1.0');
+//		}
+//  		}, function () {
+//	   	//hover out
+//	   	if ('.mainmenu ul li :not(ul.submenu)') {
+	   	//   $('ul.submenu').css({
+	   	//     'display': 'none',
+	   	//     'opacity': '0.0'
+	   	//   });
+//	    	console.log('hover out');
+//	    }
+//	  });
+//});
 
 		//display triangle if submenu exists
-		$(document).ready(function() {
-		$('.mainmenu ul li.active a').closest('ul.submenu').after('<img src="<?php echo $this->baseurl; ?>/templates/<?php echo $this->template; ?>/images/triangle.png" class="triangle"/>');
-			});
+//$(function () {
+//  $('.mainmenu ul li.active a').closest('ul.submenu').after('<img src="/adonjeschua/templates/adonjeschua/images/triangle.png" class="triangle"/>');
+//});
+</script>
